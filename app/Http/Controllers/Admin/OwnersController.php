4ef93548d4fc0fe4f36f05ab772c64064ab3eb6a
https://github.com/TaiseiUgawa <taisei8808@gmai.com>
2022-06-06 16:17:16 +0900

    edit, update

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -44,7 +44,7 @@ class OwnersController extends Controller
         // dd($e_all, $q_get, $q_first, $c_test);
 
         // Eloquentで指定したカラムデータ取得
-        $owners = Owner::select('name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
         return view('admin.owners.index',
         compact('owners'));
     }
@@ -86,7 +86,7 @@ class OwnersController extends Controller
 
         // 登録後のリダイレクト処理
         return redirect()->route('admin.owners.index')
-        ->with('message', 'オーナー登録を実施しました。'); //フラッシュメーセージ　Vue.js実装検討
+        ->with('message', 'オーナー登録を実施しました。'); //フラッシュメーセージ　Vue.js実装検討4
 
     }
 
@@ -109,7 +109,11 @@ class OwnersController extends Controller
      */
     public function edit($id)
     {
-        //
+        // $idがあれば代入　なければ 404
+        $owner = Owner::findOrFail($id);
+        // dd($owner);
+
+        return view('admin.owners.edit', compact('owner'));
     }
 
     /**
@@ -122,6 +126,15 @@ class OwnersController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $owner = Owner::findOrFail($id);
+        $owner->name = $request->name;
+        $owner->email = $request->email;
+        $owner->password = Hash::make($request->password);
+        $owner->save();
+
+        return redirect()->route('admin.owners.index')
+        ->with('message', 'オーナー情報を更新しました。');
+
     }
 
     /**
--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,10 +1,13 @@
 @props(['status' => 'info'])
+{{-- switch文でも実装可能 --}}
 
+{{-- ステータスの状態によって背景色変更 --}}
 @php
     if($status === 'info'){ $bgColor = 'bg-blue-300';}
     if($status === 'error'){ $bgColor = 'bg-red-500';}
 @endphp
 
+{{-- セッションでメッセージ来たら --}}
 @if(session('message'))
     <div class="{{ $bgColor }} w-1/2 mx-auto p-2 text-white">
         {{ session('message') }}
