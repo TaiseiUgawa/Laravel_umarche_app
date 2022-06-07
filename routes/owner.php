dc5fc0f588b891d66e3b065930a1797f16a6c7e7
https://github.com/TaiseiUgawa <taisei8808@gmai.com>
2022-06-07 23:48:01 +0900

    image初期設定

--- routes/owner.php
+++ routes/owner.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
 
 
 /*
@@ -34,6 +35,9 @@ middleware('auth:owners')->group(function(){
     Route::post('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
 
 });
+
+Route::resource('images', ImagesController::class)
+->middleware('auth:owners')->except(['show']);
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
