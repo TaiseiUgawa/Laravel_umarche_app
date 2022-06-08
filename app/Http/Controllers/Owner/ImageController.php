384dfcce1e593b84837cb0c310d0e7877a10cd0c
https://github.com/TaiseiUgawa <taisei8808@gmai.com>
2022-06-08 18:20:10 +0900

    image, create, validation

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Image;
+use App\Http\Requests\UploadImageRequest;
 
 class ImageController extends Controller
 {
@@ -58,6 +59,7 @@ class ImageController extends Controller
     public function create()
     {
         //
+        return view('owner.images.create');
     }
 
     /**
@@ -66,9 +68,10 @@ class ImageController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UploadImageRequest $request)
     {
         //
+        dd($request);
     }
 
     /**
