7b14542bc2cc10c7198183e8d7dc2469c02fe540
https://github.com/TaiseiUgawa <taisei8808@gmai.com>
2022-06-11 22:41:48 +0900

    show画面調整

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,10 +10,9 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex flex-wrap">
-
                             @foreach($products as $product)
                             <div class="w-1/4 p-2 mb:p-4">
-                                <a href="">
+                                <a href="{{ route('user.items.show', ['item' => $product->id]) }}">
                                 <div class="border rounded-md p-2 mb:p-4">
                                     {{-- コンポーネント --}}
                                     <x-thumbnail filename="{{ $product->filename ?? ''}}" type="products" />
