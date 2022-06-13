89724556502cb6f138ad1b0fc64773dfe63b94c1
https://github.com/TaiseiUgawa <taisei8808@gmai.com>
2022-06-13 13:40:38 +0900

    非同期処理テスト

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -10,6 +10,7 @@ use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -33,8 +34,8 @@ class ItemController extends Controller
     //
     public function index(Request $request)
     {
-        Mail::to('test@example.com')
-        ->send(new TestMail());
+        // 非同期メール送信
+        SendThanksMail::dispatch();
 
         $categories = PrimaryCategory::with('secondary')
         ->get();
