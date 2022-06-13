3d39c2e33ff0ecdce055e8eff2e7665c7452ebc0
https://github.com/TaiseiUgawa <taisei8808@gmai.com>
2022-06-13 15:39:49 +0900

    店舗側メール実装

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -10,6 +10,7 @@ use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
 use App\Services\CartService;
 use App\Jobs\SendThanksMail;
+use App\Jobs\SendOrderedMail;
 
 class CartController extends Controller
 {
@@ -67,12 +68,6 @@ class CartController extends Controller
     // 決済処理
     public function checkout()
     {
-        $items = Cart::where('user_id', Auth::id())->get();
-        $products = CartService::getItemsInCart($items);
-        $user = User::findOrFail(Auth::id());
-
-        SendThanksMail::dispatch($products, $user);
-        dd('メールテスト');
         // ログインユーザーが持っているカート情報
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
@@ -128,6 +123,14 @@ class CartController extends Controller
     // 決済完了後の処理
     public function success()
     {
+        $items = Cart::where('user_id', Auth::id())->get();
+        $products = CartService::getItemsInCart($items);
+        $user = User::findOrFail(Auth::id());
+
+        SendThanksMail::dispatch($products, $user);
+        foreach ($products as $product ) {
+            SendOrderedMail::dispatch($product, $user);
+        }
         Cart::where('user_id', Auth::id())->delete();
 
         return redirect()->route('user.items.index');
